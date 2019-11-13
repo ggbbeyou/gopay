0d9f545c4b7774c8d4d4e0f2cc19e8a8f3e9fffa
Jerry <fumingming@bilibili.com>
2019-11-13 21:11:04 +0800

    fix 临时commit

--- util.go
+++ util.go
@@ -6,7 +6,6 @@ import (
 	"encoding/xml"
 	"io"
 	"math/rand"
-	"reflect"
 	"sort"
 	"strconv"
 	"strings"
@@ -21,27 +20,17 @@ type BodyMap struct {
 	mu  sync.RWMutex
 }
 
-//设置参数
-//    value：仅支持类型 string,int,int64,float32,float64,ptr,struct,slice,map 类型
+// 设置参数
 func (bm *BodyMap) Set(key string, value interface{}) {
-	vKind := reflect.ValueOf(value).Kind()
-	switch vKind {
-	case reflect.String:
-		value = value.(string)
-	case reflect.Int:
-		value = Int2String(value.(int))
-	case reflect.Int64:
-		value = Int642String(value.(int64))
-	case reflect.Float32:
-		value = Float32ToString(value.(float32))
-	case reflect.Float64:
-		value = Float64ToString(value.(float64))
-	}
-	bm.sm.Store(key, value)
+	bm.mu.Lock()
+	bm.Map[key] = value
+	bm.mu.Unlock()
 }
 
-//获取参数
+// 获取参数
 func (bm *BodyMap) Get(key string) string {
+	bm.mu.RLock()
+	defer bm.mu.RUnlock()
 	if bm == nil {
 		return null
 	}
@@ -50,16 +39,16 @@ func (bm *BodyMap) Get(key string) string {
 		ok    bool
 		v     string
 	)
-	if value, ok = bm.sm.Load(key); !ok {
+	if value, ok = bm.Map[key]; !ok {
 		return null
 	}
 	if v, ok = value.(string); ok {
 		return v
 	}
-	return jsonToString(value)
+	return convertToString(value)
 }
 
-func jsonToString(v interface{}) (str string) {
+func convertToString(v interface{}) string {
 	if v == nil {
 		return null
 	}
@@ -70,48 +59,30 @@ func jsonToString(v interface{}) (str string) {
 	if bs, err = json.Marshal(v); err != nil {
 		return null
 	}
-	str = string(bs)
-	return
+	return string(bs)
 }
 
-//删除参数
+// 删除参数
 func (bm *BodyMap) Remove(key string) {
-	delete(bm, key)
+	bm.mu.Lock()
+	delete(bm.Map, key)
+	bm.mu.Unlock()
 }
 
 type xmlMapEntry struct {
 	XMLName xml.Name
-	Value   string `xml:",chardata"`
+	Value   interface{} `xml:",chardata"`
 }
 
 func (bm *BodyMap) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
-	if len(bm) == 0 {
+	if len(bm.Map) == 0 {
 		return nil
 	}
-	var (
-		value string
-		vKind reflect.Kind
-	)
 	if err = e.EncodeToken(start); err != nil {
 		return
 	}
-	for k, v := range bm {
-		vKind = reflect.ValueOf(v).Kind()
-		switch vKind {
-		case reflect.String:
-			value = v.(string)
-		case reflect.Int:
-			value = Int2String(v.(int))
-		case reflect.Int64:
-			value = Int642String(v.(int64))
-		case reflect.Float32:
-			value = Float32ToString(v.(float32))
-		case reflect.Float64:
-			value = Float64ToString(v.(float64))
-		default:
-			value = ""
-		}
-		e.Encode(xmlMapEntry{XMLName: xml.Name{Local: k}, Value: value})
+	for k, v := range bm.Map {
+		e.Encode(xmlMapEntry{XMLName: xml.Name{Local: k}, Value: v})
 	}
 	return e.EncodeToken(start.End())
 }
@@ -132,11 +103,13 @@ func (bm *BodyMap) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err err
 
 // ("bar=baz&foo=quux") sorted by key.
 func (bm *BodyMap) EncodeWeChatSignParams(apiKey string) string {
+	bm.mu.RLock()
+	defer bm.mu.RUnlock()
 	var (
 		buf     strings.Builder
 		keyList []string
 	)
-	for k := range bm {
+	for k := range bm.Map {
 		keyList = append(keyList, k)
 	}
 	sort.Strings(keyList)
@@ -156,30 +129,33 @@ func (bm *BodyMap) EncodeWeChatSignParams(apiKey string) string {
 
 // ("bar=baz&foo=quux") sorted by key.
 func (bm *BodyMap) EncodeAliPaySignParams() string {
+	bm.mu.RLock()
+	defer bm.mu.RUnlock()
 	var (
 		buf     strings.Builder
 		keyList []string
 	)
-	keyList = make([]string, 0, len(bm))
-	for k := range bm {
+	for k := range bm.Map {
 		keyList = append(keyList, k)
 	}
 	sort.Strings(keyList)
 	for _, k := range keyList {
-		buf.WriteString(k)
-		buf.WriteByte('=')
-		buf.WriteString(bm.Get(k))
-		buf.WriteByte('&')
+		if v := bm.Get(k); v != null {
+			buf.WriteString(k)
+			buf.WriteByte('=')
+			buf.WriteString(v)
+			buf.WriteByte('&')
+		}
 	}
 	return buf.String()[:buf.Len()-1]
 }
 
-//HttpAgent
+// HttpAgent
 func HttpAgent() (agent *gorequest.SuperAgent) {
 	return gorequest.New().TLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 }
 
-//获取随机字符串
+// 获取随机字符串
 //    length：字符串长度
 func GetRandomString(length int) string {
 	str := "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
@@ -196,26 +172,26 @@ func GetRandomString(length int) string {
 	return string(result)
 }
 
-//解析时间
+// 解析时间
 func ParseDateTime(timeStr string) (datetime time.Time) {
 	datetime, _ = time.ParseInLocation(TimeLayout, timeStr, time.Local)
 	return
 }
 
-//字符串转Float64
+// 字符串转Float64
 func String2Float64(floatStr string) (floatNum float64) {
 	floatNum, _ = strconv.ParseFloat(floatStr, 64)
 	return
 }
 
-//字符串转Float32
+// 字符串转Float32
 func String2Float32(floatStr string) (floatNum float32) {
 	float, _ := strconv.ParseFloat(floatStr, 32)
 	floatNum = float32(float)
 	return
 }
 
-//Float64转字符串
+// Float64转字符串
 //    floatNum：float64数字
 //    prec：精度位数（不传则默认float数字精度）
 func Float64ToString(floatNum float64, prec ...int) (floatStr string) {
@@ -227,7 +203,7 @@ func Float64ToString(floatNum float64, prec ...int) (floatStr string) {
 	return
 }
 
-//Float32转字符串
+// Float32转字符串
 //    floatNum：float32数字
 //    prec：精度位数（不传则默认float数字精度）
 func Float32ToString(floatNum float32, prec ...int) (floatStr string) {
@@ -239,66 +215,66 @@ func Float32ToString(floatNum float32, prec ...int) (floatStr string) {
 	return
 }
 
-//字符串转Int
+// 字符串转Int
 func String2Int(intStr string) (intNum int) {
 	intNum, _ = strconv.Atoi(intStr)
 	return
 }
 
-//字符串转Int32
+// 字符串转Int32
 func String2Int32(intStr string) (int32Num int32) {
 	intNum, _ := strconv.Atoi(intStr)
 	int32Num = int32(intNum)
 	return
 }
 
-//字符串转Int64
+// 字符串转Int64
 func String2Int64(intStr string) (int64Num int64) {
 	intNum, _ := strconv.Atoi(intStr)
 	int64Num = int64(intNum)
 	return
 }
 
-//Int转字符串
+// Int转字符串
 func Int2String(intNum int) (intStr string) {
 	intStr = strconv.Itoa(intNum)
 	return
 }
 
-//Int32转字符串
+// Int32转字符串
 func Int322String(intNum int32) (int32Str string) {
-	//10, 代表10进制
+	// 10, 代表10进制
 	int32Str = strconv.FormatInt(int64(intNum), 10)
 	return
 }
 
-//Int64转字符串
+// Int64转字符串
 func Int642String(intNum int64) (int64Str string) {
-	//10, 代表10进制
+	// 10, 代表10进制
 	int64Str = strconv.FormatInt(intNum, 10)
 	return
 }
 
-//解密填充模式（去除补全码） PKCS7UnPadding
-//解密时，需要在最后面去掉加密时添加的填充byte
+// 解密填充模式（去除补全码）PKCS7UnPadding
+// 解密时，需要在最后面去掉加密时添加的填充byte
 func PKCS7UnPadding(origData []byte) (bs []byte) {
 	length := len(origData)
-	unPaddingNumber := int(origData[length-1]) //找到Byte数组最后的填充byte 数字
+	unPaddingNumber := int(origData[length-1]) // 找到Byte数组最后的填充byte 数字
 	if unPaddingNumber <= 16 {
-		bs = origData[:(length - unPaddingNumber)] //只截取返回有效数字内的byte数组
+		bs = origData[:(length - unPaddingNumber)] // 只截取返回有效数字内的byte数组
 	} else {
 		bs = origData
 	}
 	return
 }
 
-//解密填充模式（去除补全码） PKCS5UnPadding
-//解密时，需要在最后面去掉加密时添加的填充byte
+// 解密填充模式（去除补全码）PKCS5UnPadding
+// 解密时，需要在最后面去掉加密时添加的填充byte
 func PKCS5UnPadding(origData []byte) (bs []byte) {
 	length := len(origData)
-	unPaddingNumber := int(origData[length-1]) //找到Byte数组最后的填充byte
+	unPaddingNumber := int(origData[length-1]) // 找到Byte数组最后的填充byte
 	if unPaddingNumber <= 16 {
-		bs = origData[:(length - unPaddingNumber)] //只截取返回有效数字内的byte数组
+		bs = origData[:(length - unPaddingNumber)] // 只截取返回有效数字内的byte数组
 	} else {
 		bs = origData
 	}
